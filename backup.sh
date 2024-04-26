d3f504427d5a5950ef741a27e0317567d761fbcc
challa prathyusha <cprathyusha94@gmail.com>
2024-04-26 01:57:35 -0500

    adding files to the git

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 INPUTFILE=file
 TIMESTAMP=$(date +%F-%H-%M-%S)
-OUTPUTFILE="/home/ec2-user/shellscripting01/$INPUTFILE-$TIMESTAMP"
+OUTPUTFILE=/home/ec2-user/shellscripting01/$INPUTFILE-$TIMESTAMP
 
 if [ -f $INPUTFILE ]
 then
-    echo "create a backupfile with name $'OUTPUTFILE'"
+    echo "create a backupfile with name $OUTPUTFILE"
 else
     echo "file $INPUTFILE does not exist"
     exit 1
