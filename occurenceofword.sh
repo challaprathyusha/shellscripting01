a98a8f9912bfec2c591f03209e757806fef1c0ec
challa prathyusha <cprathyusha94@gmail.com>
2024-04-26 17:52:17 -0500

    adding files to the git

--- occurenceofword.sh
+++ occurenceofword.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 SOURCEFILE="$1"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+validate(){
+if [ $1 -ne 0 ]
+then
+    echo -e "$2...$R failed $N"
+else
+    echo -e "$2....$G success $N"
+fi
+}
 
 if [ -f $SOURCEFILE ]
 then
-    echo "text file is given as input to the script"
+    echo -e "$G text file is given as input to the script $N"
 else
-    echo "Please give some text file to the script to read"
+    echo -e "$R Please give some text file to the script to read $N"
     exit 1
 fi
 
 CONTENT=$(cat $SOURCEFILE)
 echo "$CONTENT"
+validate $? "printing the content of sourcesfile on commandline is"
+
 UNIQWORDS=$(echo "$CONTENT"|tr " " "\n"| tr '[:upper:]' '[:lower:]'|sort |uniq)
+validate $? "unique words extraction from file is"
+
 for word in $UNIQWORDS
 do
     OCCURENCE=$(echo "$CONTENT"|grep -o "$word"|wc -l)
+    validate $? "occurence of each word from file is"
     echo "Wordname:$word,occurence of word:$OCCURENCE" 
 done
 
