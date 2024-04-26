749deb0d2ced40fcaceaa03293a19a359cda4638
challa prathyusha <cprathyusha94@gmail.com>
2024-04-26 18:29:34 -0500

    adding files to the git

--- diskusage.sh
+++ diskusage.sh
@@ -15,10 +15,11 @@ else
 fi
 }
 
+ USAGE=$($DISKUSAGE|awk -F " " '{print$6F}'|cut -d "%" -f1)
+ MOUNTPOINT=$($DISKUSAGE|awk -F " " '{print$NF}')
+
 while read -r line
 do
- USAGE=$(df -hT|grep xfs|awk -F " " '{print$6F}'|cut -d "%" -f1)
- MOUNTPOINT=$(df -hT|grep xfs|awk -F " " '{print$NF}')
 if [ $USAGE -ge $DISKTHRESHOLD ]
 then
     echo "$MOUNTPOINT is more than $DISKTHRESHOLD, and the usage is $USAGE"
