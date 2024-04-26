7c7b73365b6597d86d0c716d62302a19321dac55
challa prathyusha <cprathyusha94@gmail.com>
2024-04-26 00:58:06 -0500

    adding files to the git

---
+++ listallfilesindirectory.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SOURCEDIR="/tmp/files"
+#check if directory exists or not
+if [ -d $SOURCEDIR ]
+then
+    echo "the $SOURCEDIR directory exists"
+else
+    echo "Please makesure $SOURCEDIR directory exists"
+    exit 1
+fi
+
+FILES="/tmp/files/*"
