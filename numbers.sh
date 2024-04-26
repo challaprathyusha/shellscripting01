6aa690e76001b2c74df59031a75b04bf9bfde484
challa prathyusha <cprathyusha94@gmail.com>
2024-04-26 02:17:32 -0500

    adding files to the git

--- numbers.sh
+++ numbers.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+# Check if two arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <number1> <number2>"
+    exit 1
+fi
+
 NUM1=$1
 NUM2=$2
 
 ADD=$(($NUM1+$NUM2))
 SUB=$(($NUM1-$NUM2))
 MUL=$(($NUM1*$NUM2))
-DIV=$(($NUM1%$NUM2))
+DIV=$(($NUM1/$NUM2))
 echo "SUBTRACTION : $SUB"
 echo "ADDITION : $ADD"
 echo "MULTIPLICATION : $MUL"
