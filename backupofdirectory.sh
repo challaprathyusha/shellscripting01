6dfa05784e070d3855a1c2c92f649beee006f1ed
challa prathyusha <cprathyusha94@gmail.com>
2024-04-27 12:12:27 -0500

    adding files to the git

---
+++ backupofdirectory.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+SOURCEDIR=$1
+DESTINATIONDIR=$2
+
+if [ $# -eq 2 ]
+then
+    echo "$SOURCEDIR and $DESTINATIONDIR are passed"
+else 
+    echo "please pass directories as: <script-name> <sourcedir> <destinationdir>"
+    exit 1
+fi
+
+validate(){
+if [ -d $1 ]
+then
+ echo "$2...exist"
+else 
+ echo "$2....doesnot exist"
+fi
+}
+
+validate $SOURCEDIR "sourcedir"
+validate $DESTINATIONDIR "destinationdir"
+
+exitstatus(){
+    if [ $1 -eq 0 ]
+    then
+        echo "$2.....created"
+    else
+        echo "$2.....not created"
+    fi
+}
+
+TIMESTAMP="$(date +%F-%H-%M-%S)"
+Backup="$(tar czvf $SOURCEDIR.tar.gz $SOURCEDIR)"
+exitstatus $? "backupfile is"
+FINALFILE=$DESTINATIONDIR/$TIMESTAMP_$Backup
+exitstatus $? "finalfile is"
+
+echo "compressed backupfile is : $FINALFILE"
