f98b7ab6573355d9639f5e4d230998fd44b5dd57
challa prathyusha <cprathyusha94@gmail.com>
2024-04-27 01:24:56 -0500

    adding files to the git

---
+++ compressed-dir.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "enter source directory as input:"
+read SOURCEDIR
+echo "enter destination directory as input:"
+read DESTINDIR
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCEDIR ]
+then 
+    echo -e "$G the directory $SOURCEDIR exist $N"
+else
+    echo -e "$R Please make sure $SOURCEDIR exist $N"
+    exit 1
+fi 
+
+if [ -d $DESTINDIR ]
+then 
+    echo -e "$G the directory $DESTINDIR exist $N"
+else
+    echo -e "$R Please make sure $DESTINDIR exist $N"
+    exit 1
+fi 
+
